e52c324c04f1476315a0d96272a1e535b1b7430e
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-24 22:03:43 +0700

    Add project files.

---
+++ Practical3ex2/INews.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practical3ex2
+{
+    interface INews
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public float AverageRate { get; }
+       public void Display();
+    }
+}
